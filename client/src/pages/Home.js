b0366a394b1a8193ab3d9926902f7e53c3584a6d
odonovan1 <odogdonovan@gmail.com>
2023-02-17 14:12:51 -0500

    added more images to homepage and changed their styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,10 @@ const Home = () => {
     <div>
       <h1>WELCOME TO THE SPORT HQ!</h1>
       <h3>This is a place for fans of any sport to post, comment, and discuss and of the thought they have</h3>
-      <img src='https://tse1.mm.bing.net/th?id=OIP.nX4t_tZq43Du02uyqgHMSAHaB0&pid=Api&P=0'></img>
+      <div className='images'>
+        <img src='https://tse1.mm.bing.net/th?id=OIP.nX4t_tZq43Du02uyqgHMSAHaB0&pid=Api&P=0'></img>
+        <img src='https://tse2.mm.bing.net/th?id=OIP.mDJdgHQpbiiCT5cJfZycJAHaHa&pid=Api&P=0'></img>
+      </div>
     </div>
   )
 }
