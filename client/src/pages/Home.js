0cec4b8b6101edcc8690868950a56ecbe90819b7
odonovan1 <odogdonovan@gmail.com>
2023-02-17 14:03:14 -0500

    added image to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
     <div>
       <h1>WELCOME TO THE SPORT HQ!</h1>
       <h3>This is a place for fans of any sport to post, comment, and discuss and of the thought they have</h3>
+      <img src='https://tse1.mm.bing.net/th?id=OIP.nX4t_tZq43Du02uyqgHMSAHaB0&pid=Api&P=0'></img>
     </div>
   )
 }
