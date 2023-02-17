c71870e49477d5378bbf71b5460c3481fd2d1fe2
odonovan1 <odogdonovan@gmail.com>
2023-02-16 19:49:33 -0500

    set up comment section to link to page with certain id

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import Comment from "./Comment"
 
-const Comments = () => {
+const CommentSection = (props) => {
+  console.log(props)
+
   return (
     <div>
-      <h1>COMMENTS WILL GO HERE</h1>
+      <Comment />
+      <div id="posts">
+        {props.posts.map((post) => (
+          <div key={post._id} className="post">
+            <h3>Type: {post.postType}</h3>
+            <p>Message: {post.message}</p>
+
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
 
-export default Comments
+export default CommentSection
